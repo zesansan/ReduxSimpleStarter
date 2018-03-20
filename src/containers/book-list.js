5ad0f7c14f0fd291bcb1cffd2c399c0195fd6fda
Zejian Shen <zesansan@gmail.com>
2018-03-20 15:02:22 -0700

    finished app

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,16 +1,43 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
   renderList() {
     return this.props.books.map(book => {
       return (
-        <li key={book.title} className="list-group-item">
+        <li
+          key={book.title}
+          className="list-group-item"
+          onClick={() => this.props.selectBook(book)}
+        >
           {book.title}
         </li>
       );
     });
   }
+
   render() {
     return <ul className="list-group col-sm-4">{this.renderList()}</ul>;
   }
 }
+
+function mapStateToProps(state) {
+  //whatever returned will show up as props
+  //inside of BookList
+  return {
+    books: state.books
+  };
+}
+//Anything returned from this function will end up
+//as props on the BookList container
+function mapDispatchToProps(dispatch) {
+  //Whenever selectBook is called, the result should
+  //be passed to all of our reducers
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+//Promote BookList from a component to a container
+//it needs to know about this new dispatch method,
+//selectBook. Make it available as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
---
+++ src/reducers/reducer_active_book.js
@@ -0,0 +1,9 @@
+//sate argument is nota app state, only the state
+//this reducer is responsible for
+export default function(state = null, action) {
+  switch (action.type) {
+    case 'BOOK_SELECTED':
+      return action.payload;
+  }
+  return state;
+}
